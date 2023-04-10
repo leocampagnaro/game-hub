b14099392727b0f5a36eea3d18e64ec7a7720946
Leonardo Campagnaro <campagnaro.leo@gmail.com>
2023-04-10 14:41:05 +0200

    refactor query objects

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,14 +3,14 @@ import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
 interface Props {
-  selectedGenre: Genre | null
-  selectedPlatform: Platform | null
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-  const {data, error} = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const {data, error} = useGames(gameQuery);
 
   return (
     <>
